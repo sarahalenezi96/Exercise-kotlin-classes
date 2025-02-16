707306b18d65c0ea5b5c6943443de4396441d1d0
Sarah <sarahalenezi96@outlook.com>
2025-02-16 18:45:03 +0300

    Done

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,19 @@
 fun main() {
-    println("Hello World!")
+    val firstBook = Book()
+    firstBook.title = "Beloved"
+    firstBook.author = "Toni Morrison"
+    firstBook.yearPublished = 1987
+    val secondBook = BookConstructor(title="Inferno",author="Dante Alighieri",yearPublished=1320,genre="Poem")
+    secondBook.getBookInfo()
+}
+class Book(){
+    var title: String = ""
+    var author: String =""
+    var yearPublished: Int =0
+
+}
+class BookConstructor (var title: String, var author: String, var yearPublished: Int, var genre:String) { // i called it BookConstructor to avoid name conflict
+    fun getBookInfo(){
+        println("$title by $author($yearPublished), Book genre: $genre")
+    }
 }
